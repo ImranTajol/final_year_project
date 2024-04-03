6a62a8d026af1c791d02b08fbbe95b1bc81089a4
ImranTajol <imran_bk20@iluv.ums.edu.my>
2024-04-03 16:27:59 +0800

    add water plot function with db checking

--- Final_Year_Project/admin_class.php
+++ Final_Year_Project/admin_class.php
@@ -57,7 +57,43 @@ Class Action {
 
     function water_plot()
     {
-    
+        //retrieve from db
+        //date planted minus current date = time diff
+        //q: how do i determine how long to water for 7/8/etc.. days for potato/etc...??
+
+        // $current_date = date('Y-m-d');
+        $current_date = new DateTime('Y-m-d');
+
+        include "db_connect.php";
+
+        extract($_POST);
+        $plot_id = $_POST['plot_id'];   //containing which plot to water
+
+        if(empty($plot_id)) //check if empty
+        {
+            return json_encode(array("status" => "error", "message" => "Data field cannot be empty. Please insert value!"));
+        }
+
+        
+        
+        $stmt_find_diff = $conn->prepare("SELECT * FROM farm_details WHERE plot_id=?");
+        $stmt_find_diff->bind_param("s", $id);
+        $stmt_find_diff->execute();
+        $result = $stmt_find_diff->get_result(); // get the mysqli result
+        $user = $result->fetch_assoc(); // fetch data   
+        
+        $crop = $user['plant_type']."_crop";
+
+        //find date diff
+        $diff = date_diff($current_date, $user["date_plant"]);
+
+        $stmt_find_level = $conn->prepare("SELECT moisture_level FROM $crop WHERE crop_day < ? ORDER BY crop_day DESC LIMIT 1");
+        $stmt_find_level->bind_param("i", $diff);
+        $stmt_find_diff->execute();
+        $result = $stmt_find_diff->get_result(); // get the mysqli result
+        $user = $result->fetch_assoc(); // fetch data   
+
+        // SELECT `id`, `crop_day`, `moisture_level`, `date_created` FROM `potato_crop` WHERE crop_day < 21 ORDER BY crop_day DESC limit 1
 
 
     }
