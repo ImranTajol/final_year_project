113360875a12372b23a97b4663a22413a2bdae24
ImranTajol <imran_bk20@iluv.ums.edu.my>
2024-04-04 14:42:49 +0800

    water plot function to retrieve data from db using prepared stmt...with time diff

--- Final_Year_Project/admin_class.php
+++ Final_Year_Project/admin_class.php
@@ -1,5 +1,6 @@
 <?php
 
+
 Class Action {
 
     private $db;
@@ -62,12 +63,15 @@ Class Action {
         //q: how do i determine how long to water for 7/8/etc.. days for potato/etc...??
 
         // $current_date = date('Y-m-d');
-        $current_date = new DateTime('Y-m-d');
+        $current_date = new DateTime();
 
         include "db_connect.php";
 
         extract($_POST);
+
         $plot_id = $_POST['plot_id'];   //containing which plot to water
+
+        // var_dump($_POST);
 
         if(empty($plot_id)) //check if empty
         {
@@ -77,23 +81,32 @@ Class Action {
         
         
         $stmt_find_diff = $conn->prepare("SELECT * FROM farm_details WHERE plot_id=?");
-        $stmt_find_diff->bind_param("s", $id);
+        $stmt_find_diff->bind_param("s", $plot_id);
         $stmt_find_diff->execute();
         $result = $stmt_find_diff->get_result(); // get the mysqli result
         $user = $result->fetch_assoc(); // fetch data   
+
         
         $crop = $user['plant_type']."_crop";
 
-        //find date diff
-        $diff = date_diff($current_date, $user["date_plant"]);
+        $date_plant = new DateTime($user["date_plant"]); //create datetime object using retrieved date (string)
+
+        //find date diff in terms of days(%d)..ex: 2024/3/19->diff(2024/4/4)->format("%d") = 16 days
+        $diff = $date_plant->diff($current_date)->format("%d");
+
 
         $stmt_find_level = $conn->prepare("SELECT moisture_level FROM $crop WHERE crop_day < ? ORDER BY crop_day DESC LIMIT 1");
-        $stmt_find_level->bind_param("i", $diff);
-        $stmt_find_diff->execute();
-        $result = $stmt_find_diff->get_result(); // get the mysqli result
+        $stmt_find_level->bind_param("s", $diff);
+        $stmt_find_level->execute();
+        $result = $stmt_find_level->get_result(); // get the mysqli result
         $user = $result->fetch_assoc(); // fetch data   
 
+
         // SELECT `id`, `crop_day`, `moisture_level`, `date_created` FROM `potato_crop` WHERE crop_day < 21 ORDER BY crop_day DESC limit 1
+
+        $conn->close();
+
+        return json_encode(array("status" => "success", "message" => json_encode($user)));
 
 
     }
